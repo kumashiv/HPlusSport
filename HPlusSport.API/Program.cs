05fabeaf3a3e80b45d12e45843d4b8e86052c3a4
shiv kumar <kumarshiv_sk@outlook.com>
2025-04-20 10:58:16 -0400

    MINIMAL API
    Post, Update, Delete added
    
    Post binded with item by adding following to single product method
    WithName("GetProduct");

--- HPlusSport.API/Program.cs
+++ HPlusSport.API/Program.cs
@@ -48,12 +48,13 @@ using (var scope = app.Services.CreateScope())
     await db.Database.EnsureCreatedAsync();
 }
 
-
+// Get All products
 app.MapGet("/products", async (ShopContext _context) =>
 {
     return await _context.Products.ToArrayAsync();
 });
 
+// Get Single product
 app.MapGet("/products/{id}", async (int id, ShopContext _context) =>
 {
     var product = await _context.Products.FindAsync(id);
@@ -62,13 +63,73 @@ app.MapGet("/products/{id}", async (int id, ShopContext _context) =>
         return Results.NotFound();
     }
     return Results.Ok(product);
-});
+}).WithName("GetProduct");      // To get this with POST Product method in following 
 
-
+// Get available products
 app.MapGet("/products/available", async (ShopContext _context) =>
 
     Results.Ok(await _context.Products.Where(p => p.IsAvailable).ToListAsync())
     //Results.Ok(await _context.Products.Where(p => p.IsAvailable).ToArrayAsync())
 );
+
+
+// Chapter 3
+// Post Product
+app.MapPost("/products", async (ShopContext _context, Product product) =>
+{
+    _context.Products.Add(product);
+    await _context.SaveChangesAsync();
+
+    return Results.CreatedAtRoute(
+        "GetProduct",                   // Refrence to Get Product - WithName("GetProduct"); 
+        new { id = product.Id },
+        product);
+});
+
+
+// Update Product
+app.MapPut("/products/{id}", async (ShopContext _context, int id, Product product) =>
+{
+    if (id != product.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    _context.Entry(product).State = EntityState.Modified;
+
+    try
+    {
+        await _context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!_context.Products.Any(p => p.Id == id))
+        {
+            return Results.NotFound();
+        }
+        else
+        {
+            throw;
+        }
+    }
+
+    return Results.NoContent();
+});
+
+// Delete Product
+app.MapDelete("/products/{id}", async (ShopContext _context, int id) =>
+{
+    var product = await _context.Products.FindAsync(id);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    _context.Products.Remove(product);
+    await _context.SaveChangesAsync();
+
+    return Results.Ok(product);
+});
+
 
 app.Run();
